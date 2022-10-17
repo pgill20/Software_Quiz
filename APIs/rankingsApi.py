eafae4bc9250d4cd9e079ac6b0119e5bfa28b117
Troy Peele <TroyPeele2@users.noreply.github.com>
2022-10-17 10:00:15 -0700

    Creation of rankings Webpage

---
+++ APIs/rankingsApi.py
@@ -0,0 +1,11 @@
+# CREATE TABLE rankings (
+#     Rank int,
+#     FullName varchar(255),
+#     Email varchar(255), 
+#     Test varchar(255),
+#     Score int,
+#     TestId varchar(255),
+#     Employer varchar(255)
+# )
+
+# SELECT * FROM rankings
---
+++ rankingsWebpage.py
@@ -0,0 +1,8 @@
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/rankings')
+
+# def table():
+#     return render_template("table.html")
