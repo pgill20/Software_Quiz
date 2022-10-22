a00e2223d86414f7ea9bde70528e04519c78b9ed
pgill20 <paramvirr.gill@gmail.com>
2022-10-22 16:42:40 -0400

    Added changes to main.py

--- main.py
+++ main.py
@@ -1,28 +1,21 @@
-import datetime
-
+from unittest import TestCase
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def root():
-    # For the sake of example, use static information to inflate the template.
-    # This will be replaced with real information in later steps.
-    dummy_times = [datetime.datetime(2018, 1, 1, 10, 0, 0),
-                   datetime.datetime(2018, 1, 2, 10, 30, 0),
-                   datetime.datetime(2018, 1, 3, 11, 0, 0),
-                   ]
+def index():
+    return 'Software Quiz Homepage'
 
-    return render_template('index.html', times=dummy_times)
+headings = ("Rank", "Name", "Email", "Test", "Score")
+data = (
+    ("1", "John Daily", "jd@google.com", "A", "22"),
+    ("2", "Kevin Daily", "jacobd@google.com", "A", "24"),
+    ("3", "Vicky Brown", "brownv@osu.com", "A", "28")
+)
 
+@app.route('/ranking')
+def table():
+    return render_template("table.html", headings=headings, data=data)
 
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+app.run(host='0.0.0.0', port=81)
