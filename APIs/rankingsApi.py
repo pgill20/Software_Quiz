ff806477b819837e8ace1b6830f1cb2afb09f3fa
Troy Peele <TroyPeele2@users.noreply.github.com>
2022-10-25 17:06:16 -0700

    rankings Api

--- APIs/rankingsApi.py
+++ APIs/rankingsApi.py
@@ -10,29 +10,60 @@
 
 # CREATE TABLE rankings (Rank int, FullName varchar(255), Email varchar(255), Test varchar(255), Score int, TestId varchar(255), Employer varchar(255))
 
-import pymysql.cursors
 
-# Connect to the database
-connection = pymysql.connect(host='localhost',
-                             user='alice',
-                             password='s3cr3t',
-                             database='db',
-                             charset='utf8mb4',
-                             cursorclass=pymysql.cursors.DictCursor)
+#     db_conn.execute(
+#         "CREATE TABLE IF NOT EXISTS ratings "
+#         "( id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, "
+#         "genre VARCHAR(255) NOT NULL, rating FLOAT NOT NULL, "
+#         "PRIMARY KEY (id));"
+#     )
 
-with connection:
-    with connection.cursor() as cursor:
-        # Create a new record
-        sql = "INSERT INTO `users` (`email`, `password`) VALUES (%s, %s)"
-        cursor.execute(sql, ('webmaster@python.org', 'very-secret'))
+import datetime
+from urllib import request
+from google.cloud.sql.connector import Connector
+import sqlalchemy
+import pymysql
+import mysql.connector
 
-    # connection is not autocommit by default. So you must commit to save
-    # your changes.
-    connection.commit()
+# initialize Connector object
+connector = Connector()
 
-    with connection.cursor() as cursor:
-        # Read a single record
-        sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
-        cursor.execute(sql, ('webmaster@python.org',))
-        result = cursor.fetchone()
-        print(result)
+# function to return the database connection
+def getconn() -> pymysql.connections.Connection:
+    conn: pymysql.connections.Connection = connector.connect(
+        "boxwood-valve-365621:us-west1:rankings",
+        "pymysql",
+        user="peelet",
+        password="467Ranking",
+        db="RankingsDb"
+    )
+    return conn
+
+def getRankings():
+
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getconn,
+    )
+
+    with pool.connect() as db_conn:
+        result = db_conn.execute("SELECT * FROM entries").fetchall()
+        for row in result:
+            print(row)
+        print("\n")    
+    connector.close()
+
+def insertTestResults():
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getconn,
+    )
+
+    with pool.connect() as db_conn:
+        insert_stmt = sqlalchemy.text(
+        "INSERT INTO entries (guestName, content) values (:guestName, :content)",
+        )
+        db_conn.execute(insert_stmt, guestName="First Insert", content="Success")
+    connector.close()
+
+getRankings()
