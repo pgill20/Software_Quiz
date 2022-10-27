bbbd133e24d7148a18bb1ceb06a0487c4e7df8a7
Monduli <glendontus@gmail.com>
2022-10-27 15:11:02 -0400

    Added database functions to main

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import datetime
-from urllib import request
 from google.cloud.sql.connector import Connector
 import sqlalchemy
 import pymysql
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -12,20 +11,47 @@ app = Flask(__name__)
 connector = Connector()
 
 # function to return the database connection
-def getconn() -> pymysql.connections.Connection:
+def getConnCreation() -> pymysql.connections.Connection:
     conn: pymysql.connections.Connection = connector.connect(
-        "project:region:instance",
+        "boxwood-valve-365621:us-west1:rankings",
         "pymysql",
-        user="dcglendon@gmail.com",
+        user="dcglendon",
         password="password",
         db="quizstorage"
     )
+
     return conn
+
+def getQuiz():
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getConnCreation,
+    )
+
+    with pool.connect() as db_conn:
+        result = db_conn.execute("SELECT * FROM quizzes").fetchall()
+        for row in result:
+            print(row)
+        print("\n")    
+    connector.close()
+
+def insertQuiz(send_quiz):
+    pool = sqlalchemy.create_engine(
+        "mysql+pymysql://",
+        creator=getConnCreation,
+    )
+
+    with pool.connect() as db_conn:
+        insert_stmt = sqlalchemy.text(
+        "INSERT INTO quizzes (username, quiz) values (:username, :quiz)",
+        )
+        db_conn.execute(insert_stmt, username="DanTest", quiz=send_quiz)
+    connector.close()
 
 # create connection pool
 pool = sqlalchemy.create_engine(
     "mysql+pymysql://",
-    creator=getconn,
+    creator=getConnCreation,
 )
 
 @app.route('/')
@@ -47,10 +73,8 @@ def create_quiz():
 @app.route('/submit_quiz', methods=["POST"])
 def submit_quiz():
     # This route is taken from create_quiz and allows created quizzes to be submitted to the database.
-    with pool.connect() as db_conn:
-        # insert into database
-        db_conn.execute("INSERT INTO quizstorage VALUES (:user, :questions)", user="Dan", questions=request.body)
-    return render_template('quiz_submitted.html', status = "Complete")
+    insertQuiz(request.data)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
